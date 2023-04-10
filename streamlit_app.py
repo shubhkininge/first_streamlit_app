b2aa4dd9b118647643a9a5f40de177011f5cd04b
shubhkininge <130349772+shubhkininge@users.noreply.github.com>
2023-04-10 13:56:01 +0530

    Update streamlit_app.py
    added new menu from course and deleted mine

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Menu')
-streamlit.text('Coffee')
-streamlit.text('Sandwich')
-streamlit.text('Burger')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
